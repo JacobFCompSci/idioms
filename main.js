0026eedfbbc717e62c4912e779bb32552a9c78a4
JacobFCompSci <160067815+JacobFCompSci@users.noreply.github.com>
2024-03-19 12:27:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+document.getElementById("btn1").addEventListener("click", idiom1);
+
+function idiom1() {
+  let answer1 = "together";
+  alert(`${answer1}`);
+}
+
+document.getElementById("btn2").addEventListener("click", idiom2);
+
+function idiom2() {
+  let answer2 = "together";
+  alert(`${answer2}`);
+}
+
+document.getElementById("btn3").addEventListener("click", idiom3);
+
+function idiom3() {
+  let answer3 = "board";
+  alert(`${answer3}`);
+}
+
+document.getElementById("btn4").addEventListener("click", idiom4);
+
+function idiom4() {
+  let answer4 = "doubt";
+  alert(`${answer4}`);
+}
+
+document.getElementById("btn5").addEventListener("click", idiom5);
+
+function idiom5() {
+  let answer5 = "short";
+  alert(`${answer5}`);
+}
